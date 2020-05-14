0763bbfeb5dfee02b2cb7c344f9c2717452f387b
hsmtkk <hsmtkk@gmail.com>
2020-05-14 18:37:35 +0000

    fix drone config

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-DOCKER_BUILDKIT=1 docker build --tag hsmtkk/chitchat .
+DOCKER_BUILDKIT=1 docker build --file docker/webapp/Dockerfile --tag hsmtkk/chitchat:webapp .
+DOCKER_BUILDKIT=1 docker build --file docker/database/Dockerfile --tag hsmtkk/chitchat:database .
 
